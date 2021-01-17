1c5977237a20f864ddef3020ab2519ee279c0013
Avalora Fpena <=francisco.pena@avalora.com>
2021-01-17 19:16:28 -0300

    creacion de tablas sin relaciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EmpresasModule } from './empresas/empresas.module';
+import { ObrasModule } from './obras/obras.module';
+import { GerentesModule } from './gerentes/gerentes.module';
+import { AdministradorModule } from './administrador/administrador.module';
+import { TareasModule } from './tareas/tareas.module';
+import { SeriesModule } from './series/series.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    EmpresasModule,
+    ObrasModule,
+    GerentesModule,
+    AdministradorModule,
+    TareasModule,
+    SeriesModule,
+    UsuariosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
